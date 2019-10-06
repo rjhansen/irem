9c3aa0f5a505241153ed81d54d2779e377d4cb45
Robert J. Hansen <rjh@sixdemonbag.org>
2019-10-06 12:03:20 -0400

    Mild cleanup.

--- src/main.cc
+++ src/main.cc
@@ -1,43 +1,81 @@
 #include "main.h"
-#include "initialize.h"
 #include <iostream>
 #include <filesystem>
+#include <array>
 
-using std::filesystem::path;
 using Glib::RefPtr;
-using Gtk::Main;
-using Gtk::Builder;
-using Gtk::Application;
-using Gtk::Window;
-using Gtk::MenuItem;
-using std::string;
-using std::cout;
-using sigc::ptr_fun;
-using sigc::mem_fun;
+using Gtk::Main, Gtk::Builder, Gtk::Application, Gtk::Window, Gtk::MenuItem;
+using std::string, std::cout, std::array, std::make_unique, std::unique_ptr,
+    std::filesystem::path;
+using sigc::ptr_fun, sigc::mem_fun;
+using SQLite::Database;
 
 extern const std::string mainWindowXML;
-extern const string dbPath;
 
-Irem::Irem(RefPtr<Application> application) : app(application),
-    window(nullptr),
-    avatars(nullptr),
-    db(dbPath.c_str(), SQLite::OPEN_READWRITE)
+Irem::Irem(RefPtr<Application> application) : app(application)
 {
-    auto builder = Builder::create_from_string(mainWindowXML);
-    MenuItem *mi { nullptr };
-    builder->get_widget("MainWindow", window);
+    initializeDb();
+    initializeUi();
+}
 
-    builder->get_widget("connect-to", mi);
+void Irem::initializeUi()
+{
+    bldr = Builder::create_from_string(mainWindowXML);
+    MenuItem *mi { nullptr };
+    Window *window { nullptr };
+
+    bldr->get_widget("MainWindow", window);
+    this->window = unique_ptr<Window>(window);
+
+    bldr->get_widget("connect-to", mi);
     mi->signal_activate().connect(mem_fun(*this, &Irem::connectTo));
 
-    builder->get_widget("quit", mi);
+    bldr->get_widget("quit", mi);
     mi->signal_activate().connect(mem_fun(*window, &Window::close));
-    
-    builder->get_widget("worlds", mi);
+
+    bldr->get_widget("worlds", mi);
     mi->signal_activate().connect(mem_fun(*this, &Irem::showWorldEditor));
 
-    builder->get_widget("avatars", avatars);
-    avatars->signal_activate().connect(mem_fun(*this, &Irem::showAvatarEditor));
+    bldr->get_widget("avatars", mi);
+    mi->signal_activate().connect(mem_fun(*this, &Irem::showAvatarEditor));
+}
+
+void Irem::initializeDb()
+{
+    array<const char*, 4> initializers = {
+            "CREATE TABLE IF NOT EXISTS kinds "
+            "(id INTEGER PRIMARY KEY, name TEXT, UNIQUE(name))",
+            "CREATE TABLE IF NOT EXISTS worlds "
+            "(id INTEGER PRIMARY KEY, kind INTEGER, name TEXT, "
+            "host TEXT, port INTEGER, FOREIGN KEY(kind) "
+            "REFERENCES kinds(id), UNIQUE(name), UNIQUE(host, port) "
+            "ON CONFLICT REPLACE)",
+            "CREATE TABLE IF NOT EXISTS avatars (id INTEGER "
+            "PRIMARY KEY, world INTEGER, name TEXT, password TEXT, "
+            "FOREIGN KEY(world) REFERENCES worlds(id), UNIQUE(world, "
+            "name) ON CONFLICT REPLACE)"
+    };
+
+    path iremdir { getenv("HOME") };
+    path iremfile;
+
+    iremdir /= ".config";
+    iremdir /= "irem";
+    iremfile = iremdir / "irem.sqlite";
+
+    if (! is_directory(iremdir))
+        create_directories(iremdir);
+
+    if (! (exists(iremfile) && is_regular_file(iremfile))) {
+        auto dbPath = iremfile.c_str();
+        auto flags = SQLite::OPEN_READWRITE |
+                SQLite::OPEN_CREATE;
+        Database tmpDbObj(dbPath, flags);
+        for (auto stmt: initializers)
+            tmpDbObj.exec(stmt);
+    }
+    db = make_unique<Database>(iremfile.c_str(),
+            SQLite::OPEN_READWRITE);
 }
 
 void Irem::connectTo()
--- src/main.h
+++ src/main.h
@@ -1,11 +1,12 @@
 #include <gtkmm.h>
 #include <SQLiteCpp/SQLiteCpp.h>
 #include <string>
+#include <memory>
 
 class Irem {
     public:
     Irem(Glib::RefPtr<Gtk::Application> application);
-    ~Irem() { delete window; }
+    ~Irem() { }
     Gtk::Window& getWindow() { return *window; }
 
     private:
@@ -14,10 +15,12 @@ class Irem {
     void disconnectFrom();
     void showWorldEditor();
     void showAvatarEditor();
+    void initializeDb();
+    void initializeUi();
     Glib::RefPtr<Gtk::Application> app;
-    Gtk::Window* window;
-    Gtk::MenuItem* avatars;
-    SQLite::Database db;
+    Glib::RefPtr<Gtk::Builder> bldr;
+    std::unique_ptr<SQLite::Database> db;
+    std::unique_ptr<Gtk::Window> window;
 };
 
 bool initializeIrem();
